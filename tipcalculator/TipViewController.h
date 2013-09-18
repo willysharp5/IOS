7747675da48c64a47fb963f6b980d5c3d497473c
Edo Williams <edowilliams@hotmail.com>
2013-09-17 18:33:50 -0700

    First Commit

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Edo williams on 9/17/13.
+//  Copyright (c) 2013 Edo williams. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
